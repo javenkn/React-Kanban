12979a22821170ab54a3e20435f95d810f773fb0
javenkn <javenkn7@yahoo.com>
2016-08-16 00:05:20 -1000

    inner join with database tables work with many to many relationship

--- appBack.js
+++ appBack.js
@@ -4,6 +4,29 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+const db = require('./models');
+const User = db.User;
+const Card = db.Card;
+const user_card = db.user_card;
+
+Card.findAll({
+  include: [
+    {
+      model: User,
+      required: true
+    }
+  ]
+})
+.then((cards) => {
+  cards.forEach((card) => {
+    console.log(card.dataValues);
+    card.dataValues.Users.forEach((user) => {
+      console.log(user.dataValues);
+    });
+  });
+});
+
+
 // Middleware
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(function (req, res, next) {
