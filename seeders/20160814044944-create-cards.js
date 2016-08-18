860e743fe5cf5d00b93525423abd2ccbbd74d64c
javenkn <javenkn7@yahoo.com>
2016-08-17 20:29:48 -1000

    added more seeds for testing

--- seeders/20160814044944-create-cards.js
+++ seeders/20160814044944-create-cards.js
@@ -22,6 +22,33 @@ module.exports = {
       assigned_to: 'Jeff',
       createdAt: new Date(),
       updatedAt: new Date()
+    },
+    {
+      title: faker.name.jobTitle(),
+      priority: 100,
+      status: 2,
+      created_by: 'Jeff',
+      assigned_to: 'Bob',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: faker.name.jobTitle(),
+      priority: 100,
+      status: 2,
+      created_by: 'Bob',
+      assigned_to: 'Jeff',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: faker.name.jobTitle(),
+      priority: 100,
+      status: 3,
+      created_by: 'Bob',
+      assigned_to: 'Jeff',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
     ], {});
   },
--- seeders/20160816092421-create-user_cards.js
+++ seeders/20160816092421-create-user_cards.js
@@ -20,6 +20,24 @@ module.exports = {
       card_id: 2,
       createdAt: new Date(),
       updatedAt: new Date()
+    },
+    {
+      user_id: 1,
+      card_id: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      user_id: 2,
+      card_id: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      user_id: 2,
+      card_id: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
     ], {});
   },
